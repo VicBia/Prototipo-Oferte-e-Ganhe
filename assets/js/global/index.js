1788b02accd8b1f6062eab2433269b85cfff799e
Victoria Beatriz <victoriabeatrizsar@hotmail.com>
2024-10-24 09:46:38 -0300

    Prototipo Oferte e ganhe

---
+++ assets/js/global/index.js
@@ -0,0 +1,70 @@
+// Tabela alerta
+
+document.querySelectorAll("tr").forEach(function (row) {
+  row.querySelectorAll("td").forEach(function (cell) {
+    if (
+      cell.textContent.trim() === "ALERTA" ||
+      cell.textContent.trim() === "Atrasado"
+    ) {
+      row.style.backgroundColor = "#f96060";
+      row.style.color = "#fff";
+      row.style.fontWeight = "600";
+    }
+  });
+});
+
+// Menu Dropdown
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("dropdownMenu");
+  dropdown.classList.toggle("show");
+}
+
+// Modal
+
+function openModalCaduser() {
+  document.getElementById("open-modal-cadUser").classList.add("show");
+}
+
+function closeModalCaduser() {
+  document.getElementById("open-modal-cadUser").classList.remove("show");
+}
+
+function closeModalEdituser() {
+  document.getElementById("open-modal-editUser").classList.remove("show");
+}
+
+function openModalCriarPerfil() {
+  document.getElementById("open-modal-criarPerfil").classList.add("show");
+}
+
+function closeModalCriarPerfil() {
+  document.getElementById("open-modal-criarPerfil").classList.remove("show");
+}
+
+function openModalCadPerfil() {
+  document.getElementById("open-modal-cadPerfil").classList.add("show");
+}
+
+function closeModalCadPerfil() {
+  document.getElementById("open-modal-cadPerfil").classList.remove("show");
+}
+
+// Menu hamburguer
+
+var menuButton = document.getElementById("menu-hamb");
+var menu = document.getElementById("menu-hamb-section");
+var open = document.getElementById("open");
+var close = document.getElementById("close");
+
+menuButton.addEventListener("click", function () {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    open.style.display = "block";
+    close.style.display = "none";
+  } else {
+    menu.style.display = "block";
+    open.style.display = "none";
+    close.style.display = "block";
+  }
+});
---
+++ assets/js/pages/pageAdm.js
@@ -0,0 +1,213 @@
+// CRUD REGISTER
+function validateFormRegister() {
+  var name = document.getElementById("name").value.trim();
+  var email = document.getElementById("email").value.trim();
+  var loja = document.getElementById("loja").value.trim();
+  var password = document.getElementById("password").value.trim();
+  // var passwordRepeat = document.getElementById("passwordRepeat").value.trim();
+
+  if (!name || !email || !loja || !password) {
+    alert("Por favor, preencha todos os campos.");
+    return false;
+  }
+
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailPattern.test(email)) {
+    alert("Por favor, insira um e-mail válido.");
+    return false;
+  }
+
+  // if (password !== passwordRepeat) {
+  //   alert("As senhas não coincidem.");
+  //   return false;
+  // }
+
+  if (password.length < 6) {
+    alert("A senha deve ter no mínimo 6 caracteres.");
+    return false;
+  }
+
+  return true;
+}
+
+function initializeRegisterList() {
+  let registerList = JSON.parse(localStorage.getItem("registerList")) || [];
+
+  if (registerList.length === 0) {
+    registerList.push(
+      {
+        name: "Admin",
+        email: "admin@example.com",
+        loja: "Matriz",
+        password: "admin123",
+      },
+      {
+        name: "Gerente",
+        email: "gerente@example.com",
+        loja: "126",
+        password: "gerente123",
+      },
+      {
+        name: "Loja",
+        email: "loja@example.com",
+        loja: "126",
+        password: "loja123",
+      }
+    );
+    localStorage.setItem("registerList", JSON.stringify(registerList));
+  }
+}
+
+let deleteIndex;
+
+function showData() {
+  let registerList = JSON.parse(localStorage.getItem("registerList")) || [];
+
+  var html = "";
+  registerList.forEach(function (element, index) {
+    html += "<tr>";
+    html += `<td>${element.name}</td>`;
+    html += `<td>${element.email}</td>`;
+    html += `<td>${element.loja}</td>`;
+    html += "<td>" + new Date().toLocaleString() + "</td>";
+    html += '<td class="buttons">';
+    html +=
+      `
+        <button type="button" class="btn btn-primary btn-xs dt-edit" onclick="updateData(` +
+      index +
+      `)">
+          <img src="./assets/images/edit-pencil.svg" alt="Botão Editar" width="25px"/>
+        </button>
+        <button type="button" class="btn btn-danger btn-xs dt-delete" onclick="openModalDeleteUser(` +
+      index +
+      `)">
+          <img src="./assets/images/delete-trash.svg" alt="Botão Excluir" width="25px"/>
+        </button>
+      `;
+    html += "</td>";
+    html += "</tr>";
+  });
+
+  document.querySelector("#registerCRUD tbody").innerHTML = html;
+}
+
+function addData() {
+  if (validateFormRegister() == true) {
+    var name = document.getElementById("name").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var loja = document.getElementById("loja").value.trim();
+    var password = document.getElementById("password").value.trim();
+
+    let registerList = JSON.parse(localStorage.getItem("registerList")) || [];
+
+    registerList.push({
+      name: name,
+      email: email,
+      loja: loja,
+      password: password,
+    });
+    console.log(registerList);
+    localStorage.setItem("registerList", JSON.stringify(registerList));
+    document.getElementById("open-modal-cadUser").classList.remove("show");
+
+    showData();
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("loja").value = "";
+    document.getElementById("password").value = "";
+    //   document.getElementById("passwordRepeat").value = "";
+  }
+}
+
+function openModalDeleteUser(index) {
+  deleteIndex = index;
+  let registerList = JSON.parse(localStorage.getItem("registerList")) || [];
+
+  const userName = registerList[index].name;
+  document.querySelector(".containerDelete").innerHTML = `
+     <h2> Realmente deseja deletar o usuário <strong>${userName}</strong>?</h2>
+      <button type="button" id="delete" class="btn-del" onclick="deleteData()">
+        Deletar
+      </button>
+    `;
+
+  document.getElementById("open-modal-DeleteUser").classList.add("show");
+}
+
+function closeModalDeleteUser() {
+  document.getElementById("open-modal-DeleteUser").classList.remove("show");
+}
+
+function deleteData() {
+  let registerList = JSON.parse(localStorage.getItem("registerList")) || [];
+
+  registerList.splice(deleteIndex, 1);
+  localStorage.setItem("registerList", JSON.stringify(registerList));
+  document.getElementById("open-modal-DeleteUser").classList.remove("show");
+
+  showData();
+}
+
+function validateFormEdit() {
+  var name = document.getElementById("nameEdit").value.trim();
+  var email = document.getElementById("emailEdit").value.trim();
+  var loja = document.getElementById("lojaEdit").value.trim();
+  var password = document.getElementById("passwordEdit").value.trim();
+
+  if (!name || !email || !loja || !password) {
+    alert("Por favor, preencha todos os campos.");
+    return false;
+  }
+
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailPattern.test(email)) {
+    alert("Por favor, insira um e-mail válido.");
+    return false;
+  }
+
+  if (password.length < 6) {
+    alert("A senha deve ter no mínimo 6 caracteres.");
+    return false;
+  }
+
+  return true;
+}
+
+function updateData(index) {
+  document.getElementById("open-modal-editUser").classList.add("show");
+
+  let registerList = JSON.parse(localStorage.getItem("registerList")) || [];
+
+  document.getElementById("nameEdit").value = registerList[index].name;
+  document.getElementById("emailEdit").value = registerList[index].email;
+  document.getElementById("lojaEdit").value = registerList[index].loja;
+  document.getElementById("passwordEdit").value = registerList[index].password;
+
+  document.querySelector("#update").onclick = function () {
+    if (validateFormEdit() == true) {
+      registerList[index].name = document.getElementById("nameEdit").value;
+      registerList[index].email = document.getElementById("emailEdit").value;
+      registerList[index].loja = document.getElementById("lojaEdit").value;
+      registerList[index].password =
+        document.getElementById("passwordEdit").value;
+
+      localStorage.setItem("registerList", JSON.stringify(registerList));
+
+      document.getElementById("open-modal-editUser").classList.remove("show");
+
+      showData();
+
+      document.getElementById("nameEdit").value = "";
+      document.getElementById("emailEdit").value = "";
+      document.getElementById("lojaEdit").value = "";
+      document.getElementById("passwordEdit").value = "";
+    }
+  };
+  showData();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  initializeRegisterList();
+  showData();
+});
